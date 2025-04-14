447ef3e7a78fa08da5a0461d9d17bc9b664c272c
Nino Rottier <nino.rottier@etu.inp-n7.fr>
2025-04-14 14:37:45 +0200

    tkt

--- src/wls.py
+++ src/wls.py
@@ -1,2 +1,6 @@
 # Algorithms using weighted least squares (WLS) framework to compute edge-preserving decomposition
 # Author : Nino Rottier and Timothée Klein
+
+# Algorithm of edge-preserving smoothing
+# IN : input image
+# OUT :  
