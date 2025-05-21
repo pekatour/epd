844c726a04695c69664f591d222ec183739efc5a
Nino Rottier <nino.rottier@etu.inp-n7.fr>
2025-05-21 15:56:06 +0200

    metrics

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,9 @@
 from skimage import io
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
-# Function to load an image from a file path into a numpy array
-# IN : file_path (str) - Path to the image file
-#      gray (bool) - If True, load the image in grayscale
-#      verbose (bool) - If True, print image information and show the image
-# OUT : image (ndarray) - Loaded image as a numpy array
+
 def load_image(file_path, gray=False, verbose=False):
     """
     Load an image from a file path into a numpy array.
@@ -17,7 +14,7 @@ def load_image(file_path, gray=False, verbose=False):
     """
     image = io.imread(file_path, as_gray=gray)
     if gray:
-        image = image*255  # Convert to uint8 if grayscale
+        image = image*255
     if verbose:
         print(type(image))
         print("Image shape:", image.shape)
@@ -26,6 +23,7 @@ def load_image(file_path, gray=False, verbose=False):
         plt.imshow(image, cmap='gray' if gray else None)
         plt.show()
     return image
+
 
 def recadrage_dynamique(image, min_val, max_val):
     """
@@ -41,3 +39,28 @@ def recadrage_dynamique(image, min_val, max_val):
         return image
     else:
         return (((max_val - min_val) / (max_img - min_img)) * (image - min_img)).astype(np.uint8)
+    
+
+def epi(image_ref, image_filtered):
+    """
+    Compute the edge-preserving index (EPI) between two gray images.
+    IN : image_ref (ndarray) - Reference image
+         image_filtered (ndarray) - Filtered image
+    OUT : epi_value (float) - EPI value
+    """
+    sobel_x = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    sobel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+
+    gradient_ref_x = cv2.filter2D(image_ref, -1, sobel_x)
+    gradient_ref_y = cv2.filter2D(image_ref, -1, sobel_y)
+    gradient_ref = np.sqrt(gradient_ref_x**2 + gradient_ref_y**2)
+
+    gradient_filtered_x = cv2.filter2D(image_filtered, -1, sobel_x)
+    gradient_filtered_y = cv2.filter2D(image_filtered, -1, sobel_y)
+    gradient_filtered = np.sqrt(gradient_filtered_x**2 + gradient_filtered_y**2)
+
+    diff_ref = gradient_ref - np.mean(gradient_ref)
+    diff_filtered = gradient_filtered - np.mean(gradient_filtered)
+
+    epi_value = np.sum(diff_ref * diff_filtered) / (np.sqrt(np.sum(diff_ref**2)) * np.sqrt(np.sum(diff_filtered**2)))
+    return epi_value
